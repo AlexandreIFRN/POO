caefa7c8e210928b13d1ca97b504c6eb7f973b3f
ALEXANDRE CLEITON LIMA PEREGRINO <20251148060015@ifrn.local>
2025-09-25 17:27:36 -0300

    exercicios propostos

---
+++ exercicios/ex.01.ts
@@ -0,0 +1,26 @@
+//Exercicio 01
+
+
+// let raio: number = 3;
+// let altura: number = 2;
+
+// function calcular_volume(raio: number, altura: number): number{
+//     return Math.PI * (raio * raio) * altura
+// }
+
+// console.log("O volume é:", calcular_volume(raio, altura).toFixed(2))
+
+//Exercício 02
+
+// let media: number = 6
+
+// if (media < 6 ) {
+//     console.log('Reprovado')
+// }else{
+//     console.log('Aprovado')
+// }
+
+//Exercício 3
+
+
+
